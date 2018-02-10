0062c4f9fc03cc93fce910435650adb5aa9b6ccb
newtonstudio <newtonstudio@gmail.com>
2018-02-10 13:27:14 +0800

    facebooklogin

--- Day3/ci/application/views/frontend/register.php
+++ Day3/ci/application/views/frontend/register.php
@@ -32,6 +32,11 @@
 
 							<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
 
+							<fb:login-button 
+							  scope="public_profile,email"
+							  onlogin="checkLoginState();">
+							</fb:login-button>
+
 
 
 							<form method="POST" action="register_submit">
@@ -101,5 +106,39 @@
 
 		      };
 		    </script>
+
+			<script>
+			  window.fbAsyncInit = function() {
+			    FB.init({
+			      appId      : '190329078384109',
+			      cookie     : true,
+			      xfbml      : true,
+			      version    : 'v2.12'
+			    });
+			      
+			    FB.AppEvents.logPageView();   
+			      
+			  };
+
+			  (function(d, s, id){
+			     var js, fjs = d.getElementsByTagName(s)[0];
+			     if (d.getElementById(id)) {return;}
+			     js = d.createElement(s); js.id = id;
+			     js.src = "https://connect.facebook.net/en_US/sdk.js";
+			     fjs.parentNode.insertBefore(js, fjs);
+			   }(document, 'script', 'facebook-jssdk'));
+
+
+			  function checkLoginState(){
+			  	FB.getLoginStatus(function(response) {
+				    statusChangeCallback(response);
+				  });
+			  }
+
+			  function statusChangeCallback(response){
+			  	console.log(response);
+			  }
+
+			</script>
 
 			
