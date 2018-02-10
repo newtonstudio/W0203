343f161abcfeb02b866bdba4c3bdc859eecf617c
newtonstudio <newtonstudio@gmail.com>
2018-02-10 13:17:14 +0800

    solve issue

--- Day3/ci/application/views/frontend/logoutGoogle.php
+++ Day3/ci/application/views/frontend/logoutGoogle.php
@@ -5,10 +5,20 @@
 <script>
 
 window.onLoadCallback = function(){
-  var auth2 = gapi.auth2.getAuthInstance();
+	gapi.load('auth2', initSigninV2);
+}
+
+function initSigninV2() {
+    gapi.auth2.init({
+        client_id: '277763897621-sm987c9acs22fbi7k1jlprao5ctq8p0q.apps.googleusercontent.com'
+    }).then(function (authInstance) {
+       
+    	var auth2 = gapi.auth2.getAuthInstance();
 	    auth2.signOut().then(function () {
 	      location.href='<?=base_url('logoutStep2')?>';
 	    });
+
+    });
 }
 
 
