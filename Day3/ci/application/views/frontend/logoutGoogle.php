acbcd3e9312b5236640736ffdb312e86c9654cee
newtonstudio <newtonstudio@gmail.com>
2018-02-10 13:15:15 +0800

    async

--- Day3/ci/application/views/frontend/logoutGoogle.php
+++ Day3/ci/application/views/frontend/logoutGoogle.php
@@ -1,18 +1,15 @@
 <meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id" content="277763897621-sm987c9acs22fbi7k1jlprao5ctq8p0q.apps.googleusercontent.com">
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-<script type="text/javascript" src="<?=base_url('assets/html/template/plugins/jquery.min.js')?>"></script>
-<a href="#" onclick="signOut();">Sign out</a>
+<script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>
+
 <script>
 
-  gapi.load('client', {
-	  callback: function() {
-	    var auth2 = gapi.auth2.getAuthInstance();
+window.onLoadCallback = function(){
+  var auth2 = gapi.auth2.getAuthInstance();
 	    auth2.signOut().then(function () {
 	      location.href='<?=base_url('logoutStep2')?>';
 	    });
-	  }
-  });
+}
 
 
 </script>
